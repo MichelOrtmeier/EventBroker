c7d439ef5b95b4bf77d7f9d6cdfd64a1de4d100c
Morti <morti.mo@outlook.de>
2023-07-08 22:14:53 +0200

    Created IEnumerableIgnoringOrderComparer and implemented IEqualityComparer

---
+++ EventBroker/IEnumerableIgnoringOrderComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBroker
+{
+    internal class IEnumerableIgnoringOrderComparer<TIEnumerableContent> : IEqualityComparer<IEnumerable<TIEnumerableContent>>
+    {
+        public bool Equals(IEnumerable<TIEnumerableContent> x, IEnumerable<TIEnumerableContent> y)
+        {
+            return OrderIEnumerableByHashCode(x).SequenceEqual(OrderIEnumerableByHashCode(y));
+        }
+
+        public int GetHashCode([DisallowNull] IEnumerable<TIEnumerableContent> obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        private static IEnumerable<TIEnumerableContent> OrderIEnumerableByHashCode(IEnumerable<TIEnumerableContent> enumerable)
+        {
+            return enumerable.OrderBy((obj) => obj?.GetHashCode());
+        }
+    }
+}
