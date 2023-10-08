117862ce5fcdc3d3e3ee73384322e15f3a8f6b8d
Morti <morti.mo@outlook.de>
2023-10-08 21:25:39 +0200

    Fixed bug in MethodInfoParameterExtensions: Derived versions of required input parameters were not accepted beforehand in "HasEqualParameterTypesTo". Therefore, the method is now called "ArePassableToMethod", which was also changed in connected classes.

---
+++ EventBroker/CentralBroker.cs
@@ -0,0 +1,14 @@
+﻿using EventBroker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal static class CentralBroker
+    {
+        public static Broker broker;
+    }
+}
