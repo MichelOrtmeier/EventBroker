96fea27297c4f66a53e285a4cd99ad6d803097a0
Morti <morti.mo@outlook.de>
2023-10-08 21:26:22 +0200

    Added console program for basic tests.

---
+++ Test/Publicatioin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventBroker.EventAttributes;
+
+namespace Test
+{
+    internal class Publication
+    {
+        [EventPublication("test")]
+        public event EventHandler e;
+
+        public void Test()
+        {
+            e(this, new EventArgs());
+            Console.WriteLine("invoked");
+        }
+
+        public Publication()
+        {
+            CentralBroker.broker.Register(this);
+        }
+    }
+}
