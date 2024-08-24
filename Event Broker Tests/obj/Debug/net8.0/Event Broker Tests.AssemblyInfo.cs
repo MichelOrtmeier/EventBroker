9b4e9d1214cb48c06235a8508b146c7b09bcbecf
Morti <morti.mo@outlook.de>
2024-08-24 17:20:03 +0200

    pull on local version:changes in assembly definitions

--- Event Broker Tests/obj/Debug/net8.0/Event Broker Tests.AssemblyInfo.cs
+++ Event Broker Tests/obj/Debug/net8.0/Event Broker Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Event Broker Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9874b4d24541459c6bcb3119c092e4d16d779b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad5a29d9cfb1013aef16117310bacc2ade28c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Event Broker Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Event Broker Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Test/obj/Release/net6.0/Test.AssemblyInfo.cs
+++ Test/obj/Release/net6.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad5a29d9cfb1013aef16117310bacc2ade28c44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
